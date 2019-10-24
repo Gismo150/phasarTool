135c2bd1cd3e1311cdc4a0bf94a97af58a46829e
Gismo150 <braun.daniel@hotmail.de>
2019-10-24 15:57:31 +0200

    result file write/read handlers added. analysis now collects all basic statistics found by the GeneralStatistisPass

--- StatisticsAnalysis.cpp
+++ StatisticsAnalysis.cpp
@@ -4,6 +4,7 @@
 #include <phasar/DB/ProjectIRDB.h>
 #include <phasar/PhasarLLVM/Passes/GeneralStatisticsPass.h>
 #include <phasar/Utils/Logger.h>
+#include <boost/filesystem.hpp>
 
 
 
@@ -11,33 +12,89 @@ namespace bpo = boost::program_options;
 
 using namespace std;
 using namespace psr;
+using json = nlohmann::json;
+
+void appendModuleResult(GeneralStatisticsPass *GSP, json &outerJsonArray, llvm::Module &module) {
+    json o;
+    o["Source Filename"] = module.getSourceFileName();
+    o["Module Name"] = module.getName().str();
+    o["Allocated Types"] = GSP->getAllocatedTypes().size();
+    o["Allocated Sites"] = GSP->getAllocationsites();
+    o["Basic Blocks"] = GSP->getBasicBlocks();
+    o["Call Sites"] = GSP->getFunctioncalls(); // Call sites = how often is a function called?
+    o["Functions"] = GSP->getFunctions(); // functions = how many functions do we have?
+    o["Globals"] = GSP->getGlobals();
+    o["Global Pointer"] = GSP->getGlobalPointers();
+    o["Instructions"] = GSP->getInstructions();
+    o["Memory Intrinsics"] = GSP->getMemoryIntrinsics();
+    o["Store Instructions"] = GSP->getStoreInstructions();
+
+    std::cout << "PRINTING ANALYSIS SUMMARY FOR MODULE:" <<  module.getName().str() << "\n";
+    std::cout << o.dump(0) << std::endl;
+    outerJsonArray.push_back(o);
+}
+
+/**
+ * Checks if results.json file exists. If not return empty array. Otherwise read in the existing json array.
+ * @param outerJsonArray
+ * @return
+ */
+void readResultsJson(json &outerJsonArray, string path) {
+    if ( !boost::filesystem::exists( path ) ) //check if file
+    {
+        outerJsonArray = json::array(); // create empty array
+    } else {
+        std::ifstream i(path);
+        i >> outerJsonArray;
+    }
+}
+
+/**
+ * Writes the complete analysis result into the results.json array.
+ * @param outerJsonArray
+ */
+void writeResultsJson(json &outerJsonArray, string path) {
+    std::ofstream o(path);
+    o << outerJsonArray;
+}
 
 int main(int argc, const char **argv) {
+
     initializeLogger(false);
     auto &lg = lg::get();
-    if (argc < 2 || !bfs::exists(argv[1]) || bfs::is_directory(argv[1])) {
-        std::cerr << "usage: <prog><ir file>\n";
-        if(!bfs::is_directory(argv[1])) {
-            std::cerr << "ARGUMENT IS NOT A DIR\n";
+    if (argc < 3 || !bfs::exists(argv[1]) || bfs::is_directory(argv[1]) || !bfs::exists(argv[2])) { //TODO CHECK IF CONDITION IS CORRECT
+        std::cerr << "usage: <prog><ir file><path to results.json file>\n";
+        if(!bfs::exists(argv[1])) {
+            std::cerr << argv[1] << " IS NOT AN EXISTING FILE\n";
         }
+        if(!bfs::exists(argv[2])) {
+            std::cerr << argv[2] << " IS NOT AN EXISTING DIR\n";
+        }
+
         return 1;
     }
+    std::string path = argv[2];
+    path += "/results.json";
+    std::cout << "Writing results to: "<< path << "\n";
 
-    initializeLogger(false);
+    GeneralStatisticsPass *GSP = new GeneralStatisticsPass();
+    json outerJsonArray;
+    readResultsJson(outerJsonArray, path);
+
     ProjectIRDB DB({argv[1]}, IRDBOptions::WPA);
     DB.preprocessIR();
-
     std::set<llvm::Module *> moduleSet =  DB.getAllModules();
 
-    std::cout << "IR File contains " << moduleSet.size() << " Modules\n";
-
+    std::cout << "IR File contains " << moduleSet.size() << " Module(s)\n";
     for(auto module: moduleSet){
         std::cout << "Starting to analyse module: " << module->getName().str() << "\n";
+        GSP->runOnModule(*module);
+        GSP->doFinalization(*module);
 
-        GeneralStatisticsPass().doInitialization(*module);
-        GeneralStatisticsPass().doFinalization(*module);
+        appendModuleResult(GSP, outerJsonArray, *module);
+
     }
-
-    std:cout << "Analysis finished.\n";
+    writeResultsJson(outerJsonArray, path);
+    std::cout << "Analysis finished.\n";
     return 0;
 }
